8b0ab873c19bd8523f928475007a7fd2d3a1f58a
Berkay Dinç <berkayydinc@gmail.com>
2023-12-07 11:11:59 +0300

    create a tab list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import './App.css';
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-        hi mom
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <main>
+        <Booker />
+      </main>
+    </ThemeProvider>
   );
 }
 
+function Booker() {
+  const [activeTab, setActiveTab] = useState('1');
+
+  const handleChange = (event, newValue) => {
+    setActiveTab(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '1100px', typography: 'body1' }}>
+      <TabContext value={activeTab}>
+        <Box>
+          <TabList onChange={handleChange} variant='fullWidth'>
+            <Tab label="Flights" value="1" />
+            <Tab label="Booking" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+      </TabContext>
+    </Box>
+  );
+}
 export default App;
